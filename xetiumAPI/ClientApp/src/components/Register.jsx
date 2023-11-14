3829d5e217a4ab309115fc7efcd958298d8b6f3e
nenichv <alena_volkova_2004@mail.ru>
2023-11-14 14:57:11 +0500

    Настройка форм

--- xetiumAPI/ClientApp/src/components/Register.jsx
+++ xetiumAPI/ClientApp/src/components/Register.jsx
@@ -14,25 +14,26 @@ const Register = () => {
     return (
         <div>
         <form className="register-Field" id = "form">
-
             <div className="register-Field-left">
                 <div className="register-Field-left-header">
                     <h1>Регистрация</h1>
                     <p>Уже есть аккаунт? <a href="#">Войти</a></p>
                 </div>
 
-                <input placeholder="Имя пользователя" type="text" value = { name } maxLength = "20" id = "name" pattern = "[A-Za-z]{5,20}" title = "Минимум 5 символов" required
+                <input placeholder = "Имя пользователя" type = "text" value = { name } maxLength = "20" id = "name" pattern = "[A-Za-z]{5,20}" 
+                title = "Минимум 5 символов" required
                        onChange = {event => setName(event.target.value)} />
                 
-                <input placeholder = "Е-mail" type = "email" name = "email" pattern = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required />
+                <input placeholder = "Е-mail" type = "email" name = "email" pattern = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required 
+                    onChange = {event => setName(event.target.value)}/>
                 
                 <input placeholder = "Пароль" type = "password" id = "password" name = "password" minLength = "8" maxLength = "16"
-                       value = {password} /*pattern = ""/*"/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20}/" */
+                       value = {password} pattern = "/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20}/" 
                        title = "Длина от 8 до 16 символов, должна быть заглавная буква и спец.символ" required
                        onChange = {event => setPassword(event.target.value)}/>
 
                 <input placeholder = "Повторите пароль" type = "password" id = "secpass" name = "secpassword" minLength = "8"
-                       maxLength = "16" value = {secPassword} /*pattern = ""/*"/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20}/" */
+                       maxLength = "16" value = {secPassword} pattern = "/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20}/"
                        title = "Пароли не совпадают" required
                        onChange = {event => setSecPassword(event.target.value)}/>
                 
@@ -47,19 +48,16 @@ const Register = () => {
                 <div>
                     <input type="checkbox" id="checkboxConf" name="checkboxConf" value = {true} required
                     onChange = {event => setCheckboxConf(checkboxConf = !checkboxConf)}/>
-                    <label htmlFor="checkboxConf">Согласен с условиями пользования и политикой конфиденциальности
-                    </label>
+                    <label htmlFor="checkboxConf">Согласен с условиями пользования и политикой конфиденциальности</label>
                 </div>
                 <div>
                     <input type="checkbox" id="checkboxSpam" name="checkboxSpam" value = {true}
                     onChange = {event => setCheckboxSpam(checkboxSpam = !checkboxSpam)}/>
-                    <label htmlFor="checkboxSpam">Хочу получать вашу информационную рассылку
-                    </label>
+                    <label htmlFor="checkboxSpam">Хочу получать вашу информационную рассылку</label>
                 </div>
             </div>
             <input placeholder = "Е-mail" type = "email" name = "email" required />
         </form>
-        sendData();
         <button id = "register-button" value = "Зарегистрироваться" onClick = {sendData}>ryjgrf</button>
         </div>)
 
@@ -101,7 +99,7 @@ const Register = () => {
     }
 
     function IsNameCorrect(name) {
-        const myRe = new RegExp("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$")
+        const myRe = new RegExp("[A-Za-z]{5,20}")
 
         if (name.trim().length === 0) {
             return false
@@ -116,7 +114,7 @@ const Register = () => {
     }
 
     function IsEmailCorrect(email) {
-        const myRe = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$");
+        const myRe = new RegExp("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$");
 
         if (email.trim().length === 0) {
             return false
@@ -130,7 +128,7 @@ const Register = () => {
     }
 
     function IsPasswordCorrect(password) {
-        const myRe = new RegExp(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20}/);
+        const myRe = new RegExp("/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20}/");
 
         if (password.trim().length === 0) {
             return false
