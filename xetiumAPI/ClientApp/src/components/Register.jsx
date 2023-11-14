6a7ff52623df54f813092056395c9deb0878405b
nenichv <alena_volkova_2004@mail.ru>
2023-11-14 14:46:29 +0500

    Настройка форм

--- xetiumAPI/ClientApp/src/components/Register.jsx
+++ xetiumAPI/ClientApp/src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import './main.css';
+import './Register-authentication.css';
 import {Await} from "react-router-dom";
 
 const Register = () => {
@@ -12,59 +12,64 @@ const Register = () => {
     let [checkboxSpam, setCheckboxSpam] = useState(false)
 
     return (
-        <section className="register-Field">
+        <div>
+        <form className="register-Field" id = "form">
+
             <div className="register-Field-left">
                 <div className="register-Field-left-header">
                     <h1>Регистрация</h1>
                     <p>Уже есть аккаунт? <a href="#">Войти</a></p>
                 </div>
 
-                <input placeholder="Имя пользователя" type="text" value={name} maxLength="20" id="name"
-                       onChange={event => setName(event.target.value)}/>
+                <input placeholder="Имя пользователя" type="text" value = { name } maxLength = "20" id = "name" pattern = "[A-Za-z]{5,20}" title = "Минимум 5 символов" required
+                       onChange = {event => setName(event.target.value)} />
+                
+                <input placeholder = "Е-mail" type = "email" name = "email" pattern = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required />
+                
+                <input placeholder = "Пароль" type = "password" id = "password" name = "password" minLength = "8" maxLength = "16"
+                       value = {password} /*pattern = ""/*"/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20}/" */
+                       title = "Длина от 8 до 16 символов, должна быть заглавная буква и спец.символ" required
+                       onChange = {event => setPassword(event.target.value)}/>
 
-                <input placeholder="Почта" type="email" value={email} name="email"
-                       onChange={event => setEmail(event.target.value)}/>
-
-                <input placeholder="Пароль" type="password" id="pass" name="password" minLength="8" maxLength="16"
-                       required
-                       value={password} onChange={event => setPassword(event.target.value)}/>
-
-                <input placeholder="Повторите пароль" type="password" id="secpass" name="secpassword" minLength="8"
-                       maxLength="16" required
-                       value={secPassword} onChange={event => setSecPassword(event.target.value)}/>
-
-                <input id="register-button" type="submit" value="Зарегистрироваться" onClick={sendData}/>
+                <input placeholder = "Повторите пароль" type = "password" id = "secpass" name = "secpassword" minLength = "8"
+                       maxLength = "16" value = {secPassword} /*pattern = ""/*"/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,20}/" */
+                       title = "Пароли не совпадают" required
+                       onChange = {event => setSecPassword(event.target.value)}/>
+                
             </div>
 
             <div className="register-Field-right">
                 <div>
-                    <input type="checkbox" id="checkboxData" name="checkboxData" onChange={event =>
-                        setCheckboxData(checkboxData = !checkboxData)}/>
+                    <input type="checkbox" id="checkboxData" name="checkboxData" value = {true} required
+                    onChange = {event => setCheckboxData(checkboxData = !checkboxData)}/>
                     <label htmlFor="checkboxData">Даю свое согласие на обработку персональных данных</label>
                 </div>
                 <div>
-                    <input type="checkbox" id="checkboxConf" onChange={event =>
-                        setCheckboxConf(checkboxConf = !checkboxConf)}/>
+                    <input type="checkbox" id="checkboxConf" name="checkboxConf" value = {true} required
+                    onChange = {event => setCheckboxConf(checkboxConf = !checkboxConf)}/>
                     <label htmlFor="checkboxConf">Согласен с условиями пользования и политикой конфиденциальности
                     </label>
                 </div>
                 <div>
-                    <input type="checkbox" id="checkboxSpam" onChange={event =>
-                        setCheckboxSpam(checkboxSpam = !checkboxSpam)}/>
+                    <input type="checkbox" id="checkboxSpam" name="checkboxSpam" value = {true}
+                    onChange = {event => setCheckboxSpam(checkboxSpam = !checkboxSpam)}/>
                     <label htmlFor="checkboxSpam">Хочу получать вашу информационную рассылку
                     </label>
                 </div>
-
             </div>
-        </section>)
+            <input placeholder = "Е-mail" type = "email" name = "email" required />
+        </form>
+        sendData();
+        <button id = "register-button" value = "Зарегистрироваться" onClick = {sendData}>ryjgrf</button>
+        </div>)
 
     async function sendData() {
-        if (IsNameCorrect(name) &&
+        if (true/*IsNameCorrect(name) &&
             IsEmailCorrect(email) &&
             IsPasswordCorrect(password) &&
             IsSecPasswordCorrect(password, secPassword) &&
             IsCheckboxDataCorrect(checkboxData) &&
-            IsCheckboxConfCorrect(checkboxConf)) {
+            IsCheckboxConfCorrect(checkboxConf)*/) {
 
             const res = {
                 name: name,
@@ -75,20 +80,23 @@ const Register = () => {
                 checkboxSpam: checkboxSpam
             }
 
+            const b = document.querySelector("#form")
+            const a = new FormData(b)
+            console.log(a);
+            console.log(b);
+
             const params = {
                 method: 'POST',
                 headers: {
                     'Content-Type': "application/problem+json; charset=utf-8"
                 },
-                body: JSON.stringify(res)
+                body: JSON.stringify(a)
             };
             console.log(res)
         
         
             let result = await fetch("register", params)
             console.log(await result.text())
-            
-            //.then(сохранить jwt);
         }
     }
 
