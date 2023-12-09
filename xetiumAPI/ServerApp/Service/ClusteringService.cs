c6cfe7819473b844b7c1f6b03bd57e4d95ad09f4
Asrom11 <asrom.11@mail.ru>
2023-12-09 18:10:01 +0500

    Update ClusteringService.cs. Using async YandexGPTmodel

--- xetiumAPI/ServerApp/Service/ClusteringService.cs
+++ xetiumAPI/ServerApp/Service/ClusteringService.cs
@@ -1,4 +1,6 @@
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using xetiumAPI.Interfaces;
 
 namespace xetiumAPI.Service;
@@ -15,37 +17,55 @@ public class ClusteringService : IClusteringService
     }
     public async Task<string> GetClusterQueriesUsingAiAsync(string querie)         
     {
-        var jsonContent = $@"{{
-                ""modelUri"": ""gpt://b1gnogno2l3gvm4bj8cg/yandexgpt/latest"",
-                ""completionOptions"": {{
-                    ""stream"": false,
-                    ""temperature"": 0.1,
-                    ""maxTokens"": 4000
-                }},
-                ""messages"": [
-                    {{
-                        ""role"": ""system"",
-                        ""text"": ""Для каждого запроса из списка определить его интент (коммерческий, информационный, обучающий, навигационный или другой).""
-                    }},
-                    {{
-                        ""role"": ""user"",
-                        ""text"": ""Список:""{querie}""
-                    }}
-                ]
-            }}";
-        var request = new HttpRequestMessage
-        {
-            Method = HttpMethod.Post,
-            RequestUri = new Uri("https://llm.api.cloud.yandex.net/foundationModels/v1/completion"),
-            Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
-        };
+           _httpClient.DefaultRequestHeaders.Add("x-folder-id", FolderId);
+           _httpClient.DefaultRequestHeaders.Add("Authorization", ApiKey);
+           
+           var data = new
+           {
+               modelUri = "gpt://b1gnogno2l3gvm4bj8cg/yandexgpt",
+               completionOptions = new
+               {
+                   stream = false,
+                   temperature = 0.1,
+                   maxTokens = "1000"
+               },
+               messages = new object[]
+               {
+                   new { role = "system", text = "Разбей список ключевых фраз на кластеры по интенту запросов, не бери фразы из примера прям вообще,  чисто из списка из ключевых слов, делай в формате примера, используй все слова из ключивых фраз.Если не получится разбить, то выведи отдельно снизу в загаловке УВЫ. Пример: скачать музыку\nлучшие песни 2023 года\nкак научиться играть на гитаре\nсайт spotify\nкупить билеты на концерт\nчто такое рок-музыка\nподписаться на музыкальный сервис\nистория музыки\nсравнить цены на наушники\nпослушать радио онлайн\nА вот как я их кластеризую по интенту запросов:\n\nКоммерческий интент: запросы, которые выражают желание пользователя купить что-то или сравнить цены, характеристики, отзывы и т.д. Примеры: купить билеты на концерт, сравнить цены на наушники.\nИнформационный интент: запросы, которые выражают потребность пользователя в получении информации о чем-то или узнать ответ на вопрос. Примеры: лучшие песни 2023 года, как научиться играть на гитаре, что такое рок-музыка, история музыки.\nНавигационный интент: запросы, которые выражают намерение пользователя перейти на определенный сайт или страницу. Примеры: сайт spotify, послушать радио онлайн.\nТранзакционный интент: запросы, которые выражают желание пользователя выполнить какое-то действие на сайте или в приложении. Примеры: скачать музыку, подписаться на музыкальный сервис. " },
+                   new { role = "user", text = $"Ключевые фраза: {querie}" }
+               }
+           };
 
-        request.Headers.Add("x-folder-id", FolderId);
-        request.Headers.Add("Authorization", ApiKey);
+           var json = JsonConvert.SerializeObject(data);
+           var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-        var response = await _httpClient.SendAsync(request);
-        var responseBody = await response.Content.ReadAsStringAsync();
+           var response = await _httpClient.PostAsync("https://llm.api.cloud.yandex.net/foundationModels/v1/completionAsync", content);
+           var responseBody = await response.Content.ReadAsStringAsync();
+           
 
-        return responseBody;
+           var jsonData = JObject.Parse(responseBody);
+           var operationId = jsonData["id"]?.ToString();
+
+           var operationResponse = await Task.Run(async () =>
+           {
+               while (true)
+               {
+                   var operationResponse = await _httpClient.GetAsync($"https://llm.api.cloud.yandex.net/operations/{operationId}");
+                   var operationResponseBody = await operationResponse.Content.ReadAsStringAsync();
+
+                   var operationJsonData = JObject.Parse(operationResponseBody);
+                   var done = operationJsonData["done"]!.ToObject<bool>();
+
+                   if (done)
+                   {
+                       return operationJsonData;
+                   }
+
+                   await Task.Delay(1000);
+               }
+           });
+
+           var text = operationResponse["response"]["alternatives"][0]["message"]["text"].ToString();
+           return text;
     }
 }
