8f78011fdb8c09e27beaf88a07a2cc7bef1ccff5
nenichv <alena_volkova_2004@mail.ru>
2023-12-11 23:33:10 +0500

    add link

--- xetiumAPI/ClientApp/src/components/UI/ReportData.jsx
+++ xetiumAPI/ClientApp/src/components/UI/ReportData.jsx
@@ -8,7 +8,7 @@ export const ReportData = () => {
         <td>11.12.23</td>
         <td>Динамика позиций сайта за интервал времени</td>
         <td>01.10.23 - 01.12.23</td>
-        <td><a href="https://vk.com/grozyler" className={Styles.link}>Скачать</a></td>
+        <td><a href="https://drive.google.com/uc?export=download&id=1DN0hyadf4e9v2tlG67eHIWlk_pi_Ttq4" className={Styles.link}>Скачать</a></td>
     </tr>))
 
     return (
