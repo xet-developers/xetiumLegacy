3805e52c46bcee0dd2334218bc1425ab19b6cd3f
Ivan <117839645+1vanRybin@users.noreply.github.com>
2024-02-18 14:36:41 +0500

    fix reports.

--- xetiumAPI/ServerApp/Models/ReportInfoDto.cs
+++ xetiumAPI/ServerApp/Models/ReportInfoDto.cs
@@ -4,6 +4,7 @@ namespace xetiumAPI.Models
 {
     public class ReportInfoDto
     {
+        public Guid ProjID { get; set; }
         [DateRange("LastDate")]
         public DateTime FirstDate { get; set; }
         public DateTime LastDate { get; set; }
--- xetiumAPI/ServerApp/Service/ReportService.cs
+++ xetiumAPI/ServerApp/Service/ReportService.cs
@@ -30,55 +30,63 @@ namespace xetiumAPI.ServerApp.Service
 
             using (var package = new ExcelPackage())
             {
-                await FillSheetAsync(projects, fileName, package, reportInfo);
+                if(await FillSheetAsync(projects, fileName, package, reportInfo) is false)
+                {
+                    return null;
+                };
             }
 
             var fs = File.Open($"{Directory.GetCurrentDirectory()}{fileName}.xlsx", FileMode.Open);
             return await Task.FromResult(fs);
         }
 
-        private static async Task FillSheetAsync(List<ProjectDal> projects, string fileName,
+        private static async Task<bool> FillSheetAsync(List<ProjectDal> projects, string fileName,
             ExcelPackage package, ReportInfoDto reportInfo)
         {
             ExcelWorksheet sheet = package.Workbook.Worksheets.Add("report");
             FillHeaders(sheet);
             var row = 3;
             var column = 2;
-            foreach (var project in projects)
+            var project = projects.Find(proj => proj.ProjID == reportInfo.ProjID);
+            var searches = project.Searches.Where(search => search.Date >= reportInfo.FirstDate && search.Date <= reportInfo.LastDate);
+            foreach (var search in searches)
             {
-                foreach (var search in project.Searches.Where(search => search.Date >= reportInfo.FirstDate && search.Date <= reportInfo.LastDate))
+                FillPreHeaders(sheet, column, search);
+
+                foreach (var result in search.KeywordResults)
                 {
-                    FillPreHeaders(sheet, column, search);
-
-                    foreach (var result in search.KeywordResults)
+                    var flag = false;
+                    for (var i = 1; i < row; i++)
                     {
-                        var flag = false;
-                        for (var i = 1; i < row; i++)
+                        if ((string)sheet.Cells[i, 1].Value == result.Text)
                         {
-                            if ((string)sheet.Cells[i, 1].Value == result.Text)
-                            {
-                                FillTable(sheet, i, column, result);
-                                flag = true;
-                            }
+                            FillTable(sheet, i, column, result);
+                            flag = true;
                         }
-
-                        if (flag)
-                        {
-                            continue;
-                        }
-
-                        sheet.Cells[row, 1].Value = result.Text;
-                        FillTable(sheet, row, column, result);
-
-                        row++;
                     }
-                    column += 2;
+
+                    if (flag)
+                    {
+                        continue;
+                    }
+
+                    sheet.Cells[row, 1].Value = result.Text;
+                    FillTable(sheet, row, column, result);
+
+                    row++;
                 }
+                column += 2;
+            }
+
+            if(column == 2)
+            {
+                return false;
             }
 
             SetStyles(sheet, row, column);
 
             await package.SaveAsAsync(new FileInfo($"{Directory.GetCurrentDirectory()}{fileName}.xlsx"));
+            return true;
         }
 
         private static void SetStyles(ExcelWorksheet sheet, int row, int column)
@@ -88,6 +96,7 @@ namespace xetiumAPI.ServerApp.Service
             sheet.Cells[2, 2, 2, column - 1].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.PeachPuff);
             sheet.Cells[2, 1, row - 1, 1].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightSkyBlue);
             sheet.Cells[3, 2, row - 1, column - 1].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+            sheet.Cells.AutoFitColumns();
 
 
             foreach (var cell in sheet.Cells[1, 1, row - 1, column - 1])
